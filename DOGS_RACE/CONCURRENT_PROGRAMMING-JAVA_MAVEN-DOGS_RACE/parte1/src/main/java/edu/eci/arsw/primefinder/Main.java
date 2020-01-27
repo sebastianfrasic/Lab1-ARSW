31f8399b159e325904ea80961f471ac13ec92dd4
Juan Sebastián Frásica G <32890741+sebastianfrasic@users.noreply.github.com>
2020-01-27 18:49:45 -0500

    Parte 2.1 finalizada
    Falta colocar la sincronización

--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,71 @@
 package edu.eci.arsw.primefinder;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
-	}
-	
+    private static PrimeFinderThread[] listaDeHilos = new PrimeFinderThread[3];
+    private static Timer timer = new Timer(5000, new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+            pausarHilos();
+            imprimirPrimos();
+            leerEnter();
+        }
+    });
+
+    public static void main(String[] args) {
+        int first = 0;
+        int last = 10000000;
+        for (int i = 0; i < 3; i++) {
+            PrimeFinderThread pft = new PrimeFinderThread(first, last);
+            pft.start();
+            listaDeHilos[i] = pft;
+            first = last + 1;
+            last += 10000000;
+        }
+        timer.start();
+        for(int i=0;i<3;i++){
+            try {
+                listaDeHilos[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Este es el resultado final...");
+        imprimirPrimos();
+    }
+
+    public static void pausarHilos() {
+        for (int i = 0; i < listaDeHilos.length; i++) {
+            listaDeHilos[i].suspend();
+        }
+    }
+
+    private static void imprimirPrimos() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(listaDeHilos[i].getPrimes());
+        }
+    }
+
+    private static void leerEnter() {
+        System.out.println("Presione Enter paraver más resultados");
+        Scanner t = new Scanner(System.in);
+        String enterkey = t.nextLine();
+        if (enterkey.isEmpty()) {
+            reanudarHilos();
+        }
+    }
+
+    private static void reanudarHilos() {
+        for (int i = 0; i < listaDeHilos.length; i++) {
+            listaDeHilos[i].resume();
+        }
+    }
+
+
+
+
 }
--- DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ DOGS_RACE/CONCURRENT_PROGRAMMING-JAVA_MAVEN-DOGS_RACE/parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -4,23 +4,23 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PrimeFinderThread extends Thread{
-
-	
 	int a,b;
 	
 	private List<Integer> primes=new LinkedList<Integer>();
 	
 	public PrimeFinderThread(int a, int b) {
 		super();
+		System.out.println("Nuevo Hilo: "+a+" hasta "+b);
 		this.a = a;
 		this.b = b;
 	}
 
+	@Override
 	public void run(){
 		for (int i=a;i<=b;i++){						
 			if (isPrime(i)){
 				primes.add(i);
-				System.out.println(i);
+				//System.out.println(i);
 			}
 		}
 		
